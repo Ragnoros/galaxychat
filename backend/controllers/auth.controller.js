0331e73932797cc21cc5cddd9abebce3dd658515
Lorontal <137430073+Lorontal@users.noreply.github.com>
2024-11-11 16:29:22 +0000

    setting up auth routes

---
+++ backend/controllers/auth.controller.js
@@ -0,0 +1,10 @@
+export const signup = (req, res) => {
+  res.send("signup user");
+  console.log("SignupUser");
+};
+export const login = (req, res) => {
+  console.log("LoginUser");
+};
+export const logout = (req, res) => {
+  console.log("LogoutUser");
+};
--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import authRoutes from "./routes/auth.routes.js";
 
 dotenv.config();
 const app = express();
@@ -9,5 +10,7 @@ const PORT = process.env.PORT || 5000;
 app.get("/", (req, res) => {
   res.send("hello orld");
 });
+
+app.use("/api/auth", authRoutes);
 
 app.listen(PORT, () => console.log(`app is running on ${PORT}`));
