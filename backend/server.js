8772e2d998b7510bfdf14e3b923016760059f4ce
Lorontal <137430073+Lorontal@users.noreply.github.com>
2024-11-11 13:11:26 +0000

    Still doing app set-up

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,13 @@
-const express = require("express")
+import express from "express";
+import dotenv from "dotenv";
 
+dotenv.config();
 const app = express();
 
-app.listen(5000, () => console.log("app is running"))
+const PORT = process.env.PORT || 5000;
+
+app.get("/", (req, res) => {
+  res.send("hello orld");
+});
+
+app.listen(PORT, () => console.log(`app is running on ${PORT}`));
