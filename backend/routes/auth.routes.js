76bf49b7c08bb06f263980487aae1b77bb27bd88
Lorontal <137430073+Lorontal@users.noreply.github.com>
2024-11-19 11:08:00 +0000

    Login endpoint completed

--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -3,7 +3,7 @@ import { login, signup, logout } from "../controllers/auth.controller.js";
 const router = express.Router();
 
 router.post("/signup", signup);
-router.get("/login", login);
+router.post("/login", login);
 router.get("/logout", logout);
 
 export default router;
