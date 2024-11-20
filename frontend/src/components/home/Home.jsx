1e63ef272a290df325901010c513e027ab0b4b4a
Lorontal <137430073+Lorontal@users.noreply.github.com>
2024-11-20 12:39:33 +0000

    ui design completed

--- frontend/src/components/home/Home.jsx
+++ frontend/src/components/home/Home.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import Sidebar from "../sidebar/Sidebar";
+import MessageContainer from "../messages/MessageContainer";
 
 const Home = () => {
   return (
-    <div className="flex sm:h-[450px] md:h-[550px] rounded-lg overflow-hidden bg-gray-400 bg-clip-padding backdrop-filter backdrop-blue-lg bg-opacity-0">
+    <div className="flex sm:h-[450px] md:h-[550px] rounded-lg overflow-hidden bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0">
       <Sidebar />
-      {/* <MessageContainer /> */}
+      <MessageContainer />
     </div>
   );
 };
--- frontend/src/components/sidebar/Conversation.jsx
+++ frontend/src/components/sidebar/Conversation.jsx
@@ -6,11 +6,20 @@ const Conversation = () => {
       <div className="flex gap-2 items-center hover:bg-sky-500 rounded p-2 py-1 cursor-pointer">
         <div className="avatar online">
           <div className="w-12 rounded-full">
-            <img src="" alt="user avatar" />
+            <img
+              src="https://avatar.iran.liara.run/username?username=carter"
+              alt="user avatar"
+            />
+          </div>
+        </div>
+        <div className="flex flex-col flex-1">
+          <div className="flex gap-3 justify-between">
+            <p className="font-bold text-gray-200">John Doe</p>
+            <span className="text-xl"></span>
           </div>
         </div>
       </div>
-      ;<div></div>;
+      <div className="divider my-0 py-0 h-1"></div>
     </>
   );
 };
