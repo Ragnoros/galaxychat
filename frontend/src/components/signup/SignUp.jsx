57a3236728a80aaf5b482a643dab251e855285db
Lorontal <137430073+Lorontal@users.noreply.github.com>
2024-11-20 13:30:07 +0000

    added in the routes

--- frontend/src/components/signup/SignUp.jsx
+++ frontend/src/components/signup/SignUp.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import GenderCheckbox from "./GenderCheckbox";
+import { Link } from "react-router-dom";
 
 const SignUp = () => {
   return (
@@ -50,12 +51,12 @@ const SignUp = () => {
             />
           </div>
           <GenderCheckbox />
-          <a
-            href="#"
+          <Link
+            to="/login"
             className="text-sm hover:underline hover:text-blue-600 mt-2 inline-block"
           >
             Already have an account?
-          </a>
+          </Link>
           <div>
             <button className="btn btn-block btn-sm mt-2 border border-slate-700">
               Sign Up
