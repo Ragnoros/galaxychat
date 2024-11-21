4cad81c5aaf89c83fe3257b0f72871f19a3430f8
Lorontal <137430073+Lorontal@users.noreply.github.com>
2024-11-21 18:22:05 +0000

    ready for deployment

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,9 @@ import messageRoutes from "./routes/message.routes.js";
 import userRoutes from "./routes/user.routes.js";
 import connectToMongoDB from "./db/connectToMongoDB.js";
 import { app, server } from "./socket/socket.js";
+import path from "path";
+
+const __dirname = path.resolve();
 
 const PORT = process.env.PORT || 5000;
 
@@ -17,6 +20,12 @@ app.use(cookieParser());
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
+
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
 
 server.listen(PORT, () => {
   connectToMongoDB();
